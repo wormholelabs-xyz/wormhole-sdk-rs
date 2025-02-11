1a2c67108d53269cdf92145ec7d801840899b170
A5 Pickle <a5-pickle@users.noreply.github.com>
2025-02-11 12:27:53 -0600

    remove cargo-publish.sh

--- cargo-publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cargo publish -p wormhole-io $@
-cargo publish -p wormhole-raw-vaas --features ruint $@
-cargo publish -p wormhole-solana-consts --features mainnet $@
-cargo publish -p wormhole-solana-utils --features anchor $@
-cargo publish -p wormhole-solana-vaas --features anchor,mainnet $@
